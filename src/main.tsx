9047441a9a9b7cf371b3e3b61b6b72eee7779749
lehoang2806 <131262327+lehoang2806@users.noreply.github.com>
2025-12-21 23:58:09 +0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,23 @@
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+
 import '~/styles/global.css'
 import '~/styles/index.css'
-import App from '~/App.tsx'
+import App from '~/App'
 import CssBaseline from '@mui/material/CssBaseline'
 import { ThemeContextProvider } from './contexts/theme/themeProvider'
-createRoot(document.getElementById('root')!).render(
-  <ThemeContextProvider>
-    <CssBaseline />
-    <App />
-  </ThemeContextProvider>
+
+const rootEl = document.getElementById('root')
+
+if (!rootEl) {
+  throw new Error('Root element with id "root" not found')
+}
+
+createRoot(rootEl).render(
+  <BrowserRouter>
+    <ThemeContextProvider>
+      <CssBaseline />
+      <App />
+    </ThemeContextProvider>
+  </BrowserRouter>
 )
